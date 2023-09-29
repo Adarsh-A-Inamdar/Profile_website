9f7511e68ed3741478123f0ded67cec35f0dbf73
Adarsh-Inamdar <120334374+Adarsh-Inamdar@users.noreply.github.com>
2023-09-29 08:47:55 +0000

    Changed CSS and JS

--- static/script.js
+++ static/script.js
@@ -1,15 +1,29 @@
-// Add animation to the header
-const header = document.querySelector('header');
-header.style.opacity = 0;
+// Add animation to the skills section
+const skillsSection = document.querySelector('section:nth-child(2) ul');
+const skillsList = Array.from(skillsSection.getElementsByTagName('li'));
 
-setTimeout(() => {
-    let opacity = 0;
-    const fadeIn = setInterval(() => {
-        if (opacity >= 1) {
-            clearInterval(fadeIn);
-        } else {
-            opacity += 0.1;
-            header.style.opacity = opacity;
-        }
-    }, 100);
-}, 500);
+let skillIndex = 0;
+
+function fadeInSkill() {
+    if (skillIndex < skillsList.length) {
+        skillsList[skillIndex].style.opacity = 0;
+        setTimeout(() => {
+            let opacity = 0;
+            const fadeIn = setInterval(() => {
+                if (opacity >= 1) {
+                    clearInterval(fadeIn);
+                    skillIndex++;
+                    setTimeout(fadeInSkill, 300);
+                } else {
+                    opacity += 0.05;
+                    skillsList[skillIndex].style.opacity = opacity;
+                }
+            }, 20);
+        }, 300);
+    }
+}
+
+// Start the skills animation when the page loads
+window.addEventListener('load', () => {
+    fadeInSkill();
+});
